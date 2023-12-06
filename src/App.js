6f299673ec2ee7118a49b12f618db45b86a2ab83
Jed Palero <raven@developer.dev>
2023-12-06 09:04:51 +0800

    Counter Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import Counter from "./Counter";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <Counter />
+    </>
   );
-};
+}
 
 export default App;
