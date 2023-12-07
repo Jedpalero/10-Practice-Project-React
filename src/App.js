323745ccd9333f46c64d72cdc8539e477eddd036
Jed Palero <raven@developer.dev>
2023-12-07 11:23:01 +0800

    Form Validation Project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import Accordion from "./Accordion";
+import Validate from "./Validate";
 import "./style.css";
-import { accordionData } from "./utils/content";
 
 function App() {
   return (
-    <div>
-      <div className="accordion">
-        {accordionData.map(({ title, content }) => (
-          // Destructure
-          <Accordion
-            title={title}
-            content={content}
-            key={Math.random() * 100}
-          />
-        ))}
-      </div>
-    </div>
+    <>
+      <Validate />
+    </>
   );
 }
 
